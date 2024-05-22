13ebb0ddb636a9ed71a005b84879ff3dfdc04282
Cobb.Zhang <Cobb.Zhang@fanruan.com>
2024-05-22 17:15:05 +0800

    update：init extension

--- extend.php
+++ extend.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Cobbz\\FlarumExtMoneyBatchTransfer;
+namespace Cobbz\FlarumExtMoneyBatchTransfer;
 
 use Flarum\Extend;
 
--- js/src/forum/index.ts
+++ js/src/forum/index.ts
@@ -1,5 +1,10 @@
 import app from 'flarum/forum/app';
+import { extend } from 'flarum/common/extend'
+import HeaderPrimary from 'flarum/forum/components/HeaderPrimary';
 
 app.initializers.add('cobbz/flarum-ext-money-batch-transfer', () => {
-  console.log('[cobbz/flarum-ext-money-batch-transfer] Hello, forum!');
+  console.log('[cobbz/flarum-ext-money-batch-transfer] Hello, forum, cobb!');
+  // extend(HeaderPrimary.prototype, 'items', function(items) {
+  //   items.add('google', <a href="https://google.com">Google</a>);
+  // });
 });
